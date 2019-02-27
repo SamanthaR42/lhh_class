825cf7379f7545d58706ce92c9763c2edddbf619
Samantha <sammy.ruth.bond@gmail.com>
2019-02-27 10:52:41 -0800

    Deal some cards face-down and deal a second row.

--- deal.js
+++ deal.js
@@ -1,7 +1,19 @@
 const columnCount = 7;
 
 function deal() {
-for(let i = 0;i < 7; i++){
-    this.columns[0].append(this.cards[0])
-}
+    let n = 0;
+    for (let i = 0; i < 7; i++) {
+        this.columns[i].append(this.cards[n])
+        if (i > 0) {
+            this.cards[n].reversed = true;
+        }
+        n++;
+    }
+    for (let i = 1; i < 7 ; i++) {
+        this.columns[i].append(this.cards[n])
+        if (i > 1) {
+            this.cards[n].reversed = true;
+        }
+        n++;
+    }
 }
