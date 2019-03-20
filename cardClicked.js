84636baf315a3036176878a639c6164fc90b2040
Samantha <sammy.ruth.bond@gmail.com>
2019-03-20 10:45:10 -0700

    Start enforcing the rules on moving cards around.

--- cardClicked.js
+++ cardClicked.js
@@ -1,3 +1,26 @@
 function cardClicked(card, column, lastCard, lastColumn) {
-    alert("You clicked " + card.name() + " in column " + column.id);
+    if (card.reversed) {
+        card.reversed = false;
+        return;
+    }
+
+    if (lastCard == null) {
+        card.selected = true;
+        this.remember(card, column);
+    }
+    else {
+        if (card.color == lastCard.color) {
+            return;
+        }
+
+        if (lastCard.value + 1 != card.value) {
+            return;
+        }
+
+        let cards = lastColumn.popFrom(lastCard);
+        column.append(cards);
+        this.forget();
+        lastCard.selected = false;
+    }
+    //te shoe killed te clicky clicky
 }
