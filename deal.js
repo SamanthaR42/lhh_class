0c01826a1f0f727aa99ced4122428f3d9f4ef5e6
Samantha <sammy.ruth.bond@gmail.com>
2019-03-20 10:43:50 -0700

    Finish dealing the cards.

--- deal.js
+++ deal.js
@@ -2,18 +2,22 @@ const columnCount = 7;
 
 function deal() {
     let n = 0;
-    for (let i = 0; i < 7; i++) {
-        this.columns[i].append(this.cards[n])
-        if (i > 0) {
-            this.cards[n].reversed = true;
+    //for (let clicky clicky = 3  )
+    for (let x = 0; x < 7; x++) {
+        for (let i = x; i < 7; i++) {
+            this.columns[i].append(this.cards[n])
+            if (i > x) {
+                this.cards[n].reversed = true;
+            }
+            n++;
         }
-        n++;
     }
-    for (let i = 1; i < 7 ; i++) {
-        this.columns[i].append(this.cards[n])
-        if (i > 1) {
-            this.cards[n].reversed = true;
-        }
-        n++;
-    }
+
+ //   for (let i = 1; i < 7; i++) {
+ //       this.columns[i].append(this.cards[n])
+ //       if (i > 1) {
+ //           this.cards[n].reversed = true;
+ //       }
+ //       n++;
+ //   }
 }
